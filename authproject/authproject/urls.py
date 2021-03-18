f49c7e5e6a1fdb0a7c293c77fad06d46d00c33a0
Sapan <sapan.das3434@gmail.com>
2021-03-18 16:42:20 +0530

    3 commit

--- authproject/authproject/urls.py
+++ authproject/authproject/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from authapp.views import home,usignup,ulogin
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home,name='home')
+  
 ]
